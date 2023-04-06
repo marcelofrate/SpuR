ad90c53634ec31b32a820337dbafc98c4bccbe11
Marcelo Frate <frate@ifsp.edu.br>
2023-04-06 15:21:50 -0300

    Update SpuR_Reader_python_mod.py

--- SpuR_Reader_python_mod.py
+++ SpuR_Reader_python_mod.py
@@ -1,5 +1,6 @@
-#SpuR - An Spectral Signature Writer System in Chipless Tags Using Software Defined Radio (SDR)
+#SpuR - A system for spectral signature recognition of chipless RFID tags using Software-Defined Radio
 #Reader - Module
+# Author: Frate, M.
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Cursor
 import numpy as np
@@ -47,7 +48,7 @@ def figuraDupla(tag,freq1, mag1, freq2, mag2):
     plt.ylim(-45, 0)
     plt.xlim(0, 6000)
     plt.grid()
-    plt.savefig('/mnt/d/SpuR/BD/Tag_Read_'+str(tag)+'.png', transparent=False)
+    plt.savefig('/mnt/d/SpuR/Tag_Read_'+str(tag)+'.png', transparent=False)
     plt.show()
     
 def figura(freq, mag):
@@ -68,7 +69,7 @@ def figura(freq, mag):
 
 def expDados(freq, mag):
 	rfidSet = pd.DataFrame({'Freq': freq, 'Mag': mag})
-	rfidSet.to_csv('/mnt/d/SpuR/BD/Tag_Read_'+str(tag)+'.csv', index = False)	
+	rfidSet.to_csv('/mnt/d/SpuR/Tag_Read_'+str(tag)+'.csv', index = False)	
 
 def calcEuc(magT, mag):
     xTemp = np.array(magT)
@@ -77,15 +78,7 @@ def calcEuc(magT, mag):
     return(distE)
     
 def compTag(tagF,tagM):
-	rfidTemp = pd.read_csv("/mnt/d/SpuR/BD/BD.csv")
-	
-	#Loop
-#	tag0M = rfidTemp['Cod'] == 0
-#	tag0M = rfidTemp[tag0M]
-#	tag0M = tag0M['Mag']
-#	tag0F = rfidTemp['Cod'] == 0
-#	tag0F = rfidTemp[tag0F]
-#	tag0F = tag0F['Freq']
+	rfidTemp = pd.read_csv("/mnt/d/SpuR/BD.csv")
 	
 	#Tag1
 	tag1M = rfidTemp['Cod'] == 1
@@ -119,34 +112,11 @@ def compTag(tagF,tagM):
 	tag4F = rfidTemp[tag4F]
 	tag4F = tag4F['Freq']
 	
-	#Tag5
-#	tag5M = rfidTemp['Cod'] == 5
-#	tag5M = rfidTemp[tag5M]
-#	tag5M = tag5M['Mag']
-#	tag5F = rfidTemp['Cod'] == 5
-#	tag5F = rfidTemp[tag5F]
-#	tag5F = tag5F['Freq']
-
-	#Tag6
-#	tag6M = rfidTemp['Cod'] == 6
-#	tag6M = rfidTemp[tag6M]
-#	tag6M = tag6M['Mag']
-#	tag6F = rfidTemp['Cod'] == 6
-#	tag6F = rfidTemp[tag6F]
-#	tag6F = tag6F['Freq']
-	
-	
-	
 	#Calcula Distância Euclediana
-#	x0 = calcEuc(tag0M, tagM)
 	x1 = calcEuc(tag1M, tagM)
 	x2 = calcEuc(tag2M, tagM)
 	x3 = calcEuc(tag3M, tagM)
 	x4 = calcEuc(tag4M, tagM)
-#	x5 = calcEuc(tag5M, tagM)
-#	x6 = calcEuc(tag6M, tagM)
-	
-	
 	
 #Verifica qual é a etiqueta
 	if (x1 < x2) and (x1 < x3) and (x1 < x4):
@@ -161,33 +131,6 @@ def compTag(tagF,tagM):
 	elif (x4 < x1) and (x4 < x2) and (x4 < x3):
 		tag = "4"
 		figuraDupla(tag, tag4F, tag4M, tagF, tagM)
-	
-	
-	
-	
-	
-
-#	if (x0 < x1) and (x0 < x2) and (x0 < x3) and (x0 < x4) and (x0 < x5) and (x0 < x6):
-#		tag = "0"
-#		figuraDupla(tag, tag0F, tag0M, tagF, tagM)
-#	elif (x1 < x0) and (x1 < x2) and (x1 < x3) and (x1 < x4) and (x1 < x5) and (x1 < x6):
-#		tag = "1"
-#		figuraDupla(tag, tag1F, tag1M, tagF, tagM)
-#	elif (x2 < x0) and (x2 < x1) and (x2 < x3) and (x2 < x4) and (x2 < x5) and (x2 < x6):
-#		tag = "2"
-#		figuraDupla(tag, tag2F, tag2M, tagF, tagM)
-#	elif (x3 < x0) and (x3 < x1) and (x3 < x2) and (x3 < x4) and (x3 < x5) and (x3 < x6):
-#		tag = "3"
-#		figuraDupla(tag, tag3F, tag3M, tagF, tagM)
-#	elif (x4 < x0) and (x4 < x1) and (x4 < x2) and (x4 < x3) and (x4 < x5) and (x4 < x6):
-#		tag = "4"
-#		figuraDupla(tag, tag4F, tag4M, tagF, tagM)
-#	elif (x5 < x0) and (x5 < x1) and (x5 < x2) and (x5 < x3) and (x5 < x4) and (x5 < x6):
-#		tag = "5"
-#		figuraDupla(tag, tag5F, tag5M, tagF, tagM)
-#	elif (x6 < x0) and (x6 < x1) and (x6 < x2) and (x6 < x3) and (x6 < x4) and (x6 < x5):
-#		tag = "6"
-#		figuraDupla(tag, tag6F, tag6M, tagF, tagM)
 
 	
 def truncate(num, n):
